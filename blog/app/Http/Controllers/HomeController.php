fcc4ffb1f73beecd4a69febe39766ac0d401a44e
Quddus1916 <quddusjunior1916@gmail.com>
2021-05-24 11:00:32 -0500

    intro to controller

--- blog/app/Http/Controllers/HomeController.php
+++ blog/app/Http/Controllers/HomeController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    function Home()
+    {
+        return "controller home method";
+    }
+
+    function About()
+    {
+        return "controller about method";
+    }
+    function contact()
+    {
+        return "controller contact method";
+    }
 }
